dc350859199ccd12aad620750ccbafb385bc3ed9
Sztyup <sztyup@gmail.com>
2018-04-09 03:17:51 +0200

    Fix view

--- views/render/action.blade.php
+++ views/render/action.blade.php
@@ -6,10 +6,10 @@
             {!! $action->getStartHtml() !!}
 
             <a
-                    @if ($action->getRoute())
-                    href="@route($action->getRoute(), $route_parameters[$actionKey])"
-                    @else
-                    href="javascript:void(0);"
+                @if ($action->getRoute())
+                href="@route($action->getRoute(), $route_parameters[$actionKey])"
+                @else
+                href="javascript:void(0);"
         @endif
         @foreach ($attributes[$actionKey] as $key => $value)
             {{ $key }}="{{ $value }}"
@@ -26,41 +26,39 @@
             @if ($action->getRoute())
                 {{ $action->getRoute() }}
             @endif
+    @else
+        <span class="{{ $action->getIcon() }}"></span> {{ $action->getLabel() }}
+        @endif
+        </a>
+        {!! $action->getEndHtml() !!}
+        @else
+            {!! $action->getStartHtml() !!}
+            <button type="button"
+                @if (isset($values[$actionKey])) value="{{ $values[$actionKey] }}" @endif
+                @foreach ($attributes[$actionKey] as $key => $value)
+                    {{ $key }}="{{ $value }}"
+                @endforeach
+                @if ($action->isConfirm())
+                    @if ($action->getConfirmMessage())
+                        onclick="return confirm('{{ $action->getConfirmMessage() }}')"
+                    @else
+                        onclick="return confirm('Biztos vagy benne?')"
+                    @endif
+                @endif
+            >
+        @if (is_null($action->getLabel()) && is_null($action->getIcon()))
+            @if ($action->getRoute())
+                {{ $action->getRoute() }}
+            @else
+                null
+            @endif
         @else
             <span class="{{ $action->getIcon() }}"></span> {{ $action->getLabel() }}
             @endif
-            </a>
+            </button>
             {!! $action->getEndHtml() !!}
-            @else
-                {!! $action->getStartHtml() !!}
-                <button type="button"
-                        @if ($value)
-                        value="{{ $value }}"
-            @endif
-            @foreach ($attributes[$actionKey] as $key => $value)
-                {{ $key }}="{{ $value }}"
-            @endforeach
-            @if ($action->isConfirm())
-                @if ($action->getConfirmMessage())
-                    onclick="return confirm('{{ $action->getConfirmMessage() }}')"
-                @else
-                    onclick="return confirm('Biztos vagy benne?')"
-                @endif
-            @endif
-            >
-            @if (is_null($action->getLabel()) && is_null($action->getIcon()))
-                @if ($action->getRoute())
-                    {{ $action->getRoute() }}
-                @else
-                    null
-                @endif
-            @else
-                <span class="{{ $action->getIcon() }}"></span> {{ $action->getLabel() }}
-                @endif
-                </button>
-                {!! $action->getEndHtml() !!}
-            @endif
         @endif
-        @endforeach
+    @endif
+@endforeach
 
-        {!! $end_html_container !!}
+{!! $end_html_container !!}
