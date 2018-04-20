1886183261af12af332a817c404b0e02165c90ad
Sztyup <sztyup@gmail.com>
2018-04-20 14:28:49 +0200

    Blade helpers

--- src/Datatable/DatatableServiceProvider.php
+++ src/Datatable/DatatableServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Sztyup\Datatable;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\View\Compilers\BladeCompiler;
 
 class DatatableServiceProvider extends ServiceProvider
 {
@@ -10,8 +11,22 @@ class DatatableServiceProvider extends ServiceProvider
     {
     }
 
-    public function boot()
+    public function boot(BladeCompiler $blade)
     {
         $this->loadViewsFrom(__DIR__ . '/../../views', 'datatables');
+
+        $blade->directive("datatablejs", function ($expression) {
+            return "<?php echo \$__env->make(" .
+                "'datatables::datatable.datatable_js'," .
+                "array_except(get_defined_vars() + ['datatable' => \$dataTable], array('__data', '__path'))" .
+                ")->render(); ?>";
+        });
+
+        $blade->directive("datatable", function ($expression) {
+            return "<?php echo \$__env->make(" .
+                "'datatables::datatable.datatable_html'," .
+                "array_except(get_defined_vars() + ['datatable' => \$dataTable], array('__data', '__path'))" .
+                ")->render(); ?>";
+        });
     }
 }
